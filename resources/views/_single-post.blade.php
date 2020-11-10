f7ef2b1721ed4d9bca48492874b8b9256976e1ae
Aaron <aarongillies88@gmail.com>
2020-11-10 14:20:19 -0500

    set up basic UI for home page and ran migration for posts to the database successfully

---
+++ resources/views/_single-post.blade.php
@@ -0,0 +1,18 @@
+<!-- container to hold posts -->
+<div class="flex p-4 border-b border-b-gray-400">
+     <!-- Avatar in this column -->
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt="avatar-of-friend-in-list"
+            class="rounded-full mr-2" 
+        />
+    </div>
+    <!-- Post itself in this column -->
+    <div>
+        <h5 class="font-bold mb-4">Placeholder Friend</h5>
+        <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+        ut labore et dolore magna aliqua. Eget gravida cum sociis natoque penatibus et magnis. Condimentum mattis 
+        pellentesque id nibh tortor id aliquet lectus proin.</p>
+    </div>
+</div>
