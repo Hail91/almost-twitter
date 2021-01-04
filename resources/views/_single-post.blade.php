65f70b224ce56a1340a8d1500dfcc5d6f9fe9c3a
Aaron <aarongillies88@gmail.com>
2021-01-04 15:48:06 -0500

    added basic implement of likes/dislikes

--- resources/views/_single-post.blade.php
+++ resources/views/_single-post.blade.php
@@ -40,6 +40,12 @@
                 <span class="ml-1 mr-8 {{ $post->likes ? 'text-blue-500' : 'text-gray-600' }}">{{ $post->likes ?: 0 }}</span>
             </form>
             <!-- Need to icon dislike icon and associated logic here -->
+            <form action="/posts/{{ $post->id }}/like" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="focus:outline-none" type="submit"><i class="focus:outline-none far fa-thumbs-down mt-4 font-thin text-gray-600 cursor-pointer hover:text-red-500"></i></button>
+                <span class="ml-1 mr-8 {{ $post->likes ? 'text-gray-600' : 'text-red-600' }}">{{ $post->likes == 0 ? 1 : 0 }}</span>
+            </form>
             <i class="focus:outline-none far fa-trash-alt mt-4 font-thin text-gray-600 cursor-pointer mr-8 hover:text-red-500"></i>
         </div>
     </div>
